005b620bcc37d44c2d22401d0375c0dc0a9f211b
tommywa-a <tomiwa95@gmail.com>
2023-03-29 14:05:57 +0100

    Header Component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
+import Header from './components/ui/Header'
 import './App.css'
+
 const App = () => {
-	return <div className='container'>Hello</div>
+	return (
+		<div className='container'>
+			<Header />
+		</div>
+	)
 }
 
 export default App
