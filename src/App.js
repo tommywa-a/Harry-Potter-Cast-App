8524aad673efdb1040b1d084c643a87e87fdbfa5
tommywa-a <tomiwa95@gmail.com>
2023-03-30 12:17:34 +0100

    useEffect & HTTP Request

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Header from './components/ui/Header'
 import './App.css'
 
 const App = () => {
+	const [items, setItems] = useState([])
+	const [isLoading, setIsloading] = useState([])
+
+	useEffect(() => {
+		const fetchItems = async () => {
+			const result = await axios(`https://hp-api.onrender.com/api/characters`)
+
+			console.log(result.data)
+			setItems(result.data)
+			setIsloading(false)
+		}
+		fetchItems()
+	}, [])
+
 	return (
 		<div className='container'>
 			<Header />
