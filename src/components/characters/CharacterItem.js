b5ca629da1e25fa95fc77cc0fb965129edd9f92a
tommywa-a <tomiwa95@gmail.com>
2023-04-08 22:19:28 +0100

    Modified CharacterItem.js

--- src/components/characters/CharacterItem.js
+++ src/components/characters/CharacterItem.js
@@ -1,33 +1,39 @@
 import React from 'react'
 
 const CharacterItem = ({ item }) => {
-  return <>
-  <h3>{item.name}</h3>
-    <div className='card'>
-      <div className='card-inner'>
-        <div className='card-front'>
-          <img src={item.image} alt='' />
-        </div>
-        <div className='card-back'>
-          <h1>{item.name}</h1>
-          <ul>
-            <li>
-              <strong>Actor Name:</strong> {item.actor}
-            </li>
-            <li>
-              <strong>Nickname:</strong> {item.alternate_names[0]} <br/> {item.alternate_names[1]}
-            </li>
-            <li>
-              <strong>Birthday:</strong> {item.dateOfBirth}
-            </li>
-            <li>
-              <strong>House:</strong> {item.house}
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
-  </>
+	return (
+		<>
+			<h3>{item.name}</h3>
+			<div className='card'>
+				<div className='card-inner'>
+					<div className='card-front'>
+						<img
+							src={item.image}
+							alt=''
+						/>
+					</div>
+					<div className='card-back'>
+						<h1>{item.name}</h1>
+						<ul>
+							<li>
+								<strong>Actor Name:</strong> {item.actor}
+							</li>
+							<li>
+								<strong>Nickname:</strong> {item.alternate_names[0]} <br />{' '}
+								{item.alternate_names[1]}
+							</li>
+							<li>
+								<strong>Birthday:</strong> {item.dateOfBirth}
+							</li>
+							<li>
+								<strong>House:</strong> {item.house}
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</>
+	)
 }
 
 export default CharacterItem
