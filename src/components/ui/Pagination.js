572ec0395e4892f94ffe032822c79ba9943d1a42
tommywa-a <tomiwa95@gmail.com>
2023-04-09 21:38:40 +0100

    modified Pagination.js

--- src/components/ui/Pagination.js
+++ src/components/ui/Pagination.js
@@ -1,23 +1,32 @@
 import React from 'react'
 
 const Pagination = ({ itemsPerPage, totalItems, paginate }) => {
-  const pageNumbers = []
+	const pageNumbers = []
 
-  for (let i = 1; i <= Math.ceil(totalItems / itemsPerPage); i++) {
-    pageNumbers.push(i)
-  }
+	for (let i = 1; i <= Math.ceil(totalItems / itemsPerPage); i++) {
+		pageNumbers.push(i)
+	}
 
-  return (
-    <nav>
-      <ul className="pagination">
-        {pageNumbers.map((number) => (
-          <li key={number} className='page-item'>
-            <a onClick={() => paginate(number)} href="!#" className='page-link'>{number}</a>
-          </li>
-        ))}
-      </ul>
-    </nav>
-  )
+	return (
+		<nav>
+			<ul className='pagination'>
+				{pageNumbers.map((number) => (
+					<li
+						key={number}
+						className='page-item'
+					>
+						<a
+							onClick={() => paginate(number)}
+							href='!#'
+							className='page-link'
+						>
+							{number}
+						</a>
+					</li>
+				))}
+			</ul>
+		</nav>
+	)
 }
 
 export default Pagination
